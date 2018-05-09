20b63034551329ebe3b96a8edff5732fa1c03a6e
Daniel Meilleur <daniel.meilleur@gmail.com>
2018-05-09 13:01:15 -0700

    commit initial

---
+++ src/app.js
@@ -0,0 +1,10 @@
+/* s8 lect 62 Refactoring Stateless Funcional components */
+/* eslint react/prop-types: 0 */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import IndecisionApp from './components/IndecisionApp'
+import 'normalize.css/normalize.css'
+import './styles/style.scss'
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/* s6 lect 57 webpack devServer */
+
+/* global module, __dirname, require */
+
+const path = require('path')
+
+module.exports = {
+  entry: './src/app.js',
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',   // quel loader utiliser
+      test: /\.js$/,  // sur quels fichier le loader doit-il s'exécuter
+      exclude: /node_modules/,  // exceptions: tout le répertoire des modules
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool:'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+  }
+}
