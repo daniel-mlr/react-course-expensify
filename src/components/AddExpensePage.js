3b8617c0114f95d0996d354a52e1cbe45b599bbf
Daniel Meilleur <daniel.meilleur@gmail.com>
2018-05-29 21:58:10 -0400

    s11 lect 108 connection form avec dispatch

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,10 +1,21 @@
+/* eslint react/prop-types: 0 */
 import React from 'react'
+import { connect } from 'react-redux'
 import ExpenseForm from './ExpenseForm'
+import { addExpense } from '../actions/expenses'
 
-const  AddExpensePage = () => (
+const  AddExpensePage = (props) => (
   <div>
     <h1>Add Expense</h1>
-    <ExpenseForm/>
+    <ExpenseForm 
+      // passing the data out of the Expense Form
+      // by calling a prop that get passed in by the parent
+      // so that ExpenseForm can be reused for the edit page
+      onSubmit={(expense) => {
+        props.dispatch(addExpense(expense))
+        props.history.push('/')
+      }}
+    />
   </div>
 )
-export default AddExpensePage
+export default connect()(AddExpensePage)
