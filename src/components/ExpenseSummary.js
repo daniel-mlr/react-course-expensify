77430afb20e4d6ce7b7ccea9f112814228ebcabb
Daniel Meilleur <daniel.meilleur@gmail.com>
2018-06-21 02:31:23 -0700

    new feature avec correction de Andrew Mead

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -3,21 +3,26 @@
 
 import React from 'react'
 import {connect} from 'react-redux'
+import numeral from 'numeral'
 import selectExpenses from '../selectors/expenses'
-import expensesTotal from '../selectors/expenses-total.js'
+import selectExpensesTotal from '../selectors/expenses-total.js'
 
 
-export const ExpenseSummary = (props) => (
-  <p>Viewing {props.expenseCount} 
-  expense{(props.expenseCount > 1 ) && 's'}&nbsp; 
-  totalling {props.expenseTotal} </p> 
-)
+export const ExpenseSummary = ({ expenseCount, expenseTotal }) => { 
+  const expWord = (expenseCount > 1 ) ? 'expenses' : 'expense'
+  const expTotal = numeral(expenseTotal / 100 ).format('$0,0.00')
+  return (
+    <div>
+      <h3>Viewing {expenseCount} { expWord } totalling {expTotal}</h3>
+    </div> 
+  )
+}
 
 const mapStateToProps = (state) => {
-  const expenses = selectExpenses(state.expenses, state.filters)
+  const visibleExpenses = selectExpenses(state.expenses, state.filters)
   return {
-    expenseCount: expenses.length,
-    expenseTotal: expensesTotal(expenses)
+    expenseCount: visibleExpenses.length,
+    expenseTotal: selectExpensesTotal(visibleExpenses)
   }
 }
 
